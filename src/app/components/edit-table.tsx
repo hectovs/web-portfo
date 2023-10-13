5e79598d24becbd4c5463fc65eb740b4c1cc2acf
hectovs <htjvans@gmail.com>
2023-10-13 01:53:58 +0100

    allow eq type selection

--- src/app/components/edit-table.tsx
+++ src/app/components/edit-table.tsx
@@ -1,70 +1,85 @@
 import { useState } from "react";
 
-export default function EditTable({data, columns, update_data} : {data : object[], columns :object[],
-    update_data:(value: null | object[] | ((prevState: null | object[]) => null | object[])) => void;}) {
-    
+export default function EditTable({ data, columns, update_data }: {
+    data: object[], columns: object[],
+    update_data: (value: null | object[] | ((prevState: null | object[]) => null | object[])) => void;
+}) {
+
     const [inputRow, setInputRow] = useState<object>({})
 
     //JSON once an input row is constructed allow this to be added to the json data  
 
 
-    
-    
-    return (
-        <div>
-            <table className="table-auto border px-8 py-4 overflow-scroll w-1/2">
-                <thead className="w-full">
-                    <tr className="w-full">
-                        {
-                            columns.map((column : object, column_index:number) => {
-                                return( 
-                                    <th className="bg-blue-100 border text-left px-2 py-4" key={"header_column" + column_index}>{column["displayName"]}</th>
-                                )
 
-                            })
-                        }
-                    </tr>
-                </thead>
-                <tbody className="w-full">
+
+    return (
+        <table className="table-fixed border px-8 py-4 w-full h-1/4">
+            <thead className="">
+                <tr className="">
                     {
-                        data.map((datum : object, data_index: number ) => { 
-                            var hasAllCols = columns.every(column => Object.keys(datum).includes(column["jsonKey"]))
-                            if(hasAllCols){
-                                return(
-                                    <tr className="w-full" key={"row"+data_index}>
-                                        {
-                                            columns.map((column: object, column_index: number )=>{
-                                                return(
-                                                    <td className="border" key={"data" + data_index + "column" + column_index}>
-                                                        <input  className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-gray-100"   type="text" readOnly value={datum[column["jsonKey"]]}/>
-                                                    </td>
-                                                )
-                                            })
-                                        }
-                                    </tr>
-                                )
-                            }
+                        columns.map((column: object, column_index: number) => {
+                            return (
+                                <th className="bg-blue-100 border text-left px-2 py-4 w-1/10" key={"header_column" + column_index}>{column["displayName"]}</th>
+                            )
+
                         })
                     }
-                    <tr className="w-full">
-                        {
-                            columns.map((column:object, column_index:number)=>{
-                                return(
-                                    <td key={"column" + column_index}>
-                                        <input  className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text"  value={inputRow["jsonKey"]} onChange={(event)=>{setInputRow({...inputRow, [column["jsonKey"]]:event.target.value })}}/>
+                    <th className="bg-blue-100 border text-left px-2 py-4" key={"header_column" + "actions"}>Actions</th>
+                </tr>
+            </thead>
+            <tbody className="">
+                {
+                    data.map((datum: object, data_index: number) => {
+                        var hasAllCols = columns.every(column => Object.keys(datum).includes(column["jsonKey"]))
+                        if (hasAllCols) {
+                            return (
+                                <tr className="" key={"row" + data_index}>
+                                    {
+                                        columns.map((column: object, column_index: number) => {
+                                            return (
+                                                <td className="border w-1/10" key={"data" + data_index + "column" + column_index}>
+                                                    <input className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-gray-100 w-full" type="text" readOnly value={datum[column["jsonKey"]]} />
+                                                </td>
+                                            )
+                                        })
+                                    }
+                                    <td className="border" key={"data" + data_index + "column" + "actions"}>
+                                        <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded" onClick={() => {
+                                            let newData = data
+                                            newData.splice(data_index, 1)
+                                            update_data([...newData])
+                                        }}>
+                                            Remove
+                                        </button>
                                     </td>
-                                )
-                            })
+
+                                </tr>
+                            )
                         }
-                    </tr>
-                </tbody>
-            </table>
-            <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded" onClick={()=>{
-                console.log(inputRow)
-                update_data([...data, inputRow])
-            }}>
-                Add row 
-            </button> 
-        </div>
+                    })
+                }
+                <tr className="">
+                    {
+                        columns.map((column: object, column_index: number) => {
+                            return (
+                                <td className="border" key={"column" + column_index}>
+                                    <input className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" value={inputRow["jsonKey"]} onChange={(event) => { setInputRow({ ...inputRow, [column["jsonKey"]]: event.target.value }) }} />
+                                </td>
+                            )
+                        })
+                    }
+                    <td className="border" key={"column" + "actions"}>
+                        <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded" onClick={() => {
+                            console.log(inputRow)
+                            let newData = [...data, inputRow].sort((a,b)=>Date.parse(a["Date"]) - Date.parse(b["Date"]))
+                            update_data(newData)
+                        }}>
+                            Add
+                        </button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+
     )
 }
--- src/app/demos/openpl/page.tsx
+++ src/app/demos/openpl/page.tsx
@@ -1,22 +1,23 @@
 "use client"
 
 import EditTable from "@/app/components/edit-table"
-import {useCallback, useEffect, useMemo, useRef, useState} from "react"
-import  Papa  from "papaparse"
+import { useCallback, useEffect, useMemo, useRef, useState } from "react"
+import Papa from "papaparse"
 import { Doughnut, Scatter } from "react-chartjs-2"
 import 'chartjs-adapter-date-fns';
 import zoomPlugin from "chartjs-plugin-zoom"
-import { Chart as ChartJS,
+import {
+    Chart as ChartJS,
     ArcElement,
     CategoryScale,
     LinearScale,
     PointElement,
     LineElement,
-    TimeScale, 
+    TimeScale,
     Title,
     Tooltip,
     Legend,
-    ChartData, 
+    ChartData,
     ChartOptions,
     DatasetChartOptions,
     ScatterController,
@@ -25,224 +26,232 @@ import { Chart as ChartJS,
 
 ChartJS.register(ArcElement, CategoryScale, LineElement, LinearScale, PointElement, TimeScale, Title, Tooltip, Legend, zoomPlugin)
 
-function randomHexColor(){
+function randomHexColor() {
     let n = (Math.random() * 0xfffff * 1000000).toString(16)
-    return "#" + n.slice(0,6); 
+    return "#" + n.slice(0, 6);
 }
 
 
-export default function openPl(){
+export default function openPl() {
 
     const scatterRef = useRef<ChartJS<"scatter">>(null)
     const doughnutRef = useRef<ChartJS<"doughnut">>(null)
 
     const [data, setData] = useState<object[]>([])
 
-    const [doughnutData, setDoughnutData] = useState<ChartData<"doughnut">>({ 
+    const [doughnutData, setDoughnutData] = useState<ChartData<"doughnut">>({
         labels: ["Attempts Made", "Attempts Missed"],
-        datasets:[]
+        datasets: []
     })
     const [isLoading, setLoading] = useState<boolean>(false)
     const [lifter, setLifter] = useState<string>("hectorvansmirren")
     const [lifterInput, setLifterInput] = useState<string>("hectorvansmirren")
     const [local, setLocal] = useState<boolean>(true) //true means local instance of openpowerlifting is accessed 
     const [scatterOptions, setScatterOptions] = useState<ChartOptions<"scatter">>({
-        responsive: true, 
-        showLine: true, 
-        plugins: { 
-            legend : { 
+        responsive: true,
+        showLine: true,
+        plugins: {
+            legend: {
                 position: 'top'
-            }, 
-            title: { 
-                display:true, 
+            },
+            title: {
+                display: true,
                 text: 'Lifts and Total over Time'
             },
             zoom: {
-                pan:{
+                pan: {
                     enabled: true,
                     mode: "xy"
                 },
                 zoom: {
-                  wheel: {
-                    enabled: true,
-                  },
-                  pinch: {
-                    enabled: true
-                  },
-                  mode: 'x',
+                    wheel: {
+                        enabled: true,
+                    },
+                    pinch: {
+                        enabled: true
+                    },
+                    mode: 'x',
                 }
             }
-        }, 
-        scales:{ 
-            x: { 
-                type : 'time', 
-                position: 'bottom', 
-                title : {
+        },
+        scales: {
+            x: {
+                type: 'time',
+                position: 'bottom',
+                title: {
                     display: true,
                     align: 'center',
                     text: 'Time'
                 },
 
-            }, 
-            y:{
+            },
+            y: {
                 beginAtZero: true,
                 type: 'linear',
-                title:{
+                title: {
                     display: true,
                     align: 'center',
                     text: 'Mass (kg)'
-                }, 
-                ticks: { 
-                    stepSize: 25, 
+                },
+                ticks: {
+                    stepSize: 25,
                 }
             }
         }
     })
     const [scatterData, setScatterData] = useState<ChartData<"scatter">>({
-        datasets:[]
+        datasets: []
     })
+    const [equipmentOptions, setEquipmentOptions] = useState<string[]>(["Raw", "Single-ply", "Multi-ply", "Wraps"])
     const [equipment, setEquipment] = useState<string[]>(["Raw"]) //Raw | Single-Ply | Multi-Ply | Raw + Wraps
     const [event, setEvent] = useState<string[]>(["SBD"]) //SBD for Squat Bench Deadlift, B for Bench only
 
-    const [chartDataDisplay, setChartDataDisplay]  = useState<object[]>([
+    const [chartDataDisplay, setChartDataDisplay] = useState<object[]>([
         {
             "jsonKey": "Best3SquatKg",
-            "displayName" : "Squat", 
+            "displayName": "Squat",
         },
         {
-            "jsonKey" : "Best3BenchKg", 
-            "displayName" : "Bench"
+            "jsonKey": "Best3BenchKg",
+            "displayName": "Bench"
         },
         {
-            "jsonKey" : "Best3DeadliftKg",
-            "displayName" : "Deadlift"
+            "jsonKey": "Best3DeadliftKg",
+            "displayName": "Deadlift"
         },
         {
-            "jsonKey" : "TotalKg", 
-            "displayName" : "Total"
+            "jsonKey": "TotalKg",
+            "displayName": "Total"
         },
         {
-            "jsonKey" : "Dots", 
-            "displayName" : "Dots"
+            "jsonKey": "Dots",
+            "displayName": "Dots"
         }
     ])
 
 
-    const [tableDataDisplay, setTableDataDisplay] =useState<object[]>([
+    const [tableDataDisplay, setTableDataDisplay] = useState<object[]>([
         {
             "jsonKey": "Best3SquatKg",
-            "displayName" : "Squat", 
+            "displayName": "Squat",
         },
         {
-            "jsonKey" : "Best3BenchKg", 
-            "displayName" : "Bench"
+            "jsonKey": "Best3BenchKg",
+            "displayName": "Bench"
         },
         {
-            "jsonKey" : "Best3DeadliftKg",
-            "displayName" : "Deadlift"
+            "jsonKey": "Best3DeadliftKg",
+            "displayName": "Deadlift"
         },
         {
-            "jsonKey" : "TotalKg", 
-            "displayName" : "Total"
+            "jsonKey": "TotalKg",
+            "displayName": "Total"
         },
         {
-            "jsonKey" : "Dots", 
-            "displayName" : "Dots"
+            "jsonKey": "Dots",
+            "displayName": "Dots"
         },
         {
-            "jsonKey" : "Date", 
-            "displayName" : "Date", 
+            "jsonKey": "Date",
+            "displayName": "Date",
         },
         {
-            "jsonKey" : "MeetName",
-            "displayName" : "Meet Name"
-        }, 
+            "jsonKey": "MeetName",
+            "displayName": "Meet Name"
+        },
         {
-            "jsonKey" : "Event",
-            "displayName" : "Event"
-        }, 
+            "jsonKey": "Event",
+            "displayName": "Event"
+        },
         {
-            "jsonKey" : "Equipment",
-            "displayName" : "equipment"
+            "jsonKey": "Equipment",
+            "displayName": "Equipment"
         }
 
     ])
 
-    const [successMetricLifts, setSuccessMetricLifts ] = useState<string[]>([
+    const [successMetricLifts, setSuccessMetricLifts] = useState<string[]>([
         "Squat1Kg", "Squat2Kg", "Squat3Kg",
-        "Bench1Kg", "Bench2Kg", "Bench3Kg", 
-        "Deadlift1Kg", "Deadlift2Kg", "Deadlift3Kg", 
-    ]) 
+        "Bench1Kg", "Bench2Kg", "Bench3Kg",
+        "Deadlift1Kg", "Deadlift2Kg", "Deadlift3Kg",
+    ])
 
-   
+
     useEffect(() => {
-      setLoading(true)
-      fetch("/api/lifterscsv/" + lifter)
-      .then((res) => res.text())
-      .then((data)=>{
-        const oplJson = Papa.parse(data, {header: true})
-        console.log(oplJson)
-        setData(oplJson.data)
-      })
-      setLoading(false)
+        setLoading(true)
+        fetch("/api/lifterscsv/" + lifter)
+            .then((res) => res.text())
+            .then((data) => {
+                const oplJson = Papa.parse(data, { header: true })
+                console.log(oplJson)
+                setData(oplJson.data.sort((a,b)=>Date.parse(a["Date"]) - Date.parse(b["Date"])))
+            })
+        setLoading(false)
     }, [lifter])
 
-    useCallback(()=>{
+    useCallback(() => {
         setLifter(lifterInput)
-    },[lifter])
+    }, [lifter])
 
-    useMemo(()=>{
+    const transformToScatter = (data: object[], equipment: string[], chartDataDisplay: object[])=>{
         //transforms data into scatterData
-        let newScatterData : ChartData<"scatter"> = {
+        let newScatterData: ChartData<"scatter"> = {
             datasets: []
-        }; 
+        };
 
-        if(data.length > 0 ){
-            chartDataDisplay.forEach((displayObj)=>{
-                var dataset = { 
-                    label: displayObj["displayName"], 
-                    borderColor:  randomHexColor(), 
-                    data:[]
+        if (data.length > 0) {
+            chartDataDisplay.forEach((displayObj) => {
+                var dataset = {
+                    label: displayObj["displayName"],
+                    borderColor: randomHexColor(),
+                    data: []
                 }
 
-                data.forEach((meet)=>{
+                data.forEach((meet) => {
                     const utc = Date.parse(meet["Date"])
-                    if(event.includes(meet["Event"]) && equipment.includes(meet["Equipment"]) ){
-                        if(displayObj["jsonKey"] in meet){
-                            if(meet[displayObj["jsonKey"]] !== ""){
-                                dataset["data"].push({y: meet[displayObj["jsonKey"]] , x: utc})
+                    if (event.includes(meet["Event"]) && equipment.includes(meet["Equipment"])) {
+                        if (displayObj["jsonKey"] in meet) {
+                            if (meet[displayObj["jsonKey"]] !== "") {
+                                dataset["data"].push({ y: meet[displayObj["jsonKey"]], x: utc })
                             }
                         }
                     }
                 })
-                dataset["data"].sort((a,b)=>(a.x-b.x))
+                dataset["data"].sort((a, b) => (a.x - b.x))
                 newScatterData.datasets.push(dataset)
             })
         }
 
         console.log(newScatterData)
         setScatterData(newScatterData)
-    },[data])
+    }
 
-    useMemo(()=>{
-        var attemptsMade : number = 0 
-        var attemptsMissed : number = 0
+    useMemo(() => {
+        transformToScatter(data, equipment, chartDataDisplay)
+
+    }, [data, equipment, chartDataDisplay])
+
+
+
+    useMemo(() => {
+        var attemptsMade: number = 0
+        var attemptsMissed: number = 0
 
         var newDoughnutDataset = {
-            backgroundColor: ["rgb(0, 255, 0)","rgb(255, 0, 0)" ],
-            borderColor: ["rgb(0, 255, 0)","rgb(255, 0, 0)" ],
+            backgroundColor: ["rgb(0, 255, 0)", "rgb(255, 0, 0)"],
+            borderColor: ["rgb(0, 255, 0)", "rgb(255, 0, 0)"],
             label: "Number of Lifts ",
-            data: [0,0],
+            data: [0, 0],
         }
-        
-        
-        if(data.length > 0 && successMetricLifts.length > 0 ){
-            data.forEach((meet)=>{
-                successMetricLifts.forEach((lift)=>{
-                    if(lift in meet){
-                        if(meet[lift] > 0 && meet[lift] !== ""){
+
+
+        if (data.length > 0 && successMetricLifts.length > 0) {
+            data.forEach((meet) => {
+                successMetricLifts.forEach((lift) => {
+                    if (lift in meet) {
+                        if (meet[lift] > 0 && meet[lift] !== "") {
                             attemptsMade++
-                        } else if(meet[lift] < 0 && meet[lift] !== "") {
+                        } else if (meet[lift] < 0 && meet[lift] !== "") {
                             attemptsMissed++
                         }
                     }
@@ -251,37 +260,56 @@ export default function openPl(){
         }
 
         newDoughnutDataset["data"] = [attemptsMade, attemptsMissed]
-        
-        setDoughnutData({...doughnutData, "datasets": [newDoughnutDataset]})
 
-    },[successMetricLifts, data])
+        setDoughnutData({ ...doughnutData, "datasets": [newDoughnutDataset] })
 
-    useEffect(()=>{
+    }, [successMetricLifts, data])
+
+    useEffect(() => {
         console.log(data)
     }, [data])
-    
-    //TODO CSS to put resetZoom button in a good place and work with screen resizes 
-    //TODO write a rust wasm to do the curve fit and therefore projection into the future based on dots 
-
-    //give the ability to alter the entries on the line chart -> create a modifiable table from the JSON 
 
     
-    if (isLoading) return <p>Loading...</p>
-    if (!data) return <p>No profile data</p>
-    
-    function resetZoom(){ 
-        if(scatterRef.current){
+    function resetZoom() {
+        if (scatterRef.current) {
             scatterRef.current.resetZoom()
         }
     }
-
-    return(
+    if (isLoading) return <p>Loading...</p>
+    if (!data) return <p>No profile data</p>
+    
+    return (
         <div className="w-full">
+            <div className="w-full py-2 px-4 flex flex-row h-16">
+                <label className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" htmlFor="lifter">
+                    Lifter Name
+                </label>
+                <input className="shadow appearance-none border rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mx-2" id="lifter" type="text" value={lifterInput} onChange={(event) => { setLifterInput(event.target.value) }} />                
+                <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded mx-2" onClick={() => { setLifter(lifterInput) }}>
+                    Update Lifter Data
+                </button>
+                <label htmlFor="equipment">Equipment Type</label>
+                <select 
+                    id="equipment"
+                    className="shadow appearance-none border rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mx-2 w-48"
+                    multiple={true}
+                    value={equipment}
+                    onChange={(event)=>{
+                        const options = [...event.target.selectedOptions];
+                        const values = options.map(option => option.value)
+                        setEquipment(values)
+                    }}
+                >
+                    {equipmentOptions.map((eO)=>{ 
+                        return(<option key={eO} value={eO}>{eO}</option>)
+                    })}
+                </select>
+            </div>
             <div className="w-full py-2 px-4 flex flex-row relative">
                 <div className="w-1/2 left-0 flex relative">
                     <div className="grow absolute top-0 right-0 py-6">
                         <button onClick={resetZoom} className="absolute top-0 right-0 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-2 border border-blue-500 hover:border-transparent rounded">
-                            Reset Zoom 
+                            Reset
                         </button>
                     </div>
                     <div className="w-full">
@@ -293,17 +321,9 @@ export default function openPl(){
                 </div>
             </div>
 
-            <div className="w-full max-w-xs py-2 px-4">
-                <input className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" value={lifterInput} onChange={(event)=>{setLifterInput(event.target.value)}} />
-                <div className="py-2">
-                    <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded" onClick={()=>{setLifter(lifterInput)}}>
-                        Update Lifter Data
-                    </button> 
-                </div>
+            <div className="w-full h-1/4 overflow-y-auto">
+                <EditTable columns={tableDataDisplay} data={data} update_data={setData} />
             </div>
-
-            <EditTable columns={tableDataDisplay} data={data} update_data={setData} />
-
         </div>
     )
 }
